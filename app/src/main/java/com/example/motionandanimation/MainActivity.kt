f7f1d92bca479a19794a67a2be64d9a3ea3e36cb
jayesh <jayeshseth45@gmail.com>
2020-10-19 20:51:00 +0530

    added Suggestion List

--- app/src/main/java/com/example/motionandanimation/MainActivity.kt
+++ app/src/main/java/com/example/motionandanimation/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.motionandanimation
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import kotlinx.android.synthetic.main.activity_home.*
+import java.lang.Math.abs
 
 class MainActivity : AppCompatActivity() {
     private var productlist = ArrayList<Products>()
@@ -52,12 +52,12 @@ class MainActivity : AppCompatActivity() {
         viewPager.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
         val compositePageTransformer = CompositePageTransformer()
         compositePageTransformer.addTransformer(MarginPageTransformer(40))
-//        compositePageTransformer.addTransformer { page, position ->
-//            val r:Float = 1 - abs(position)
-//            page.scaleY = 0.85f + r * 0.15f
-//        }
-        viewPager.setPageTransformer(SliderTransformer(3))
-//        viewPager.setPageTransformer(compositePageTransformer)
+        compositePageTransformer.addTransformer { page, position ->
+            val r:Float = 1 - kotlin.math.abs(position)
+            page.scaleY = 0.85f + r * 0.15f
+        }
+//        viewPager.setPageTransformer(SliderTransformer(3))
+        viewPager.setPageTransformer(compositePageTransformer)
         /*
             setting up Based on purchase list
          */
